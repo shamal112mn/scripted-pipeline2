1181daba034e22f4fcdf33b892cf903ddc97dd62
azat <azatm@gmail.com>
2020-12-26 17:37:35 +0000

    key2

--- ansible-flaskex.groovy
+++ ansible-flaskex.groovy
@@ -9,18 +9,18 @@ node{
         git url: 'https://github.com/ikambarov/ansible-Flaskex.git'
     }
     stage("Install Prerequisites"){
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'prerequisites.yml'
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'prerequisites.yml'
     }
     withEnv(['FLASKEX_REPO=https://github.com/ikambarov/Flaskex.git', 'FLASKEX_BRANCH=master']) {
         stage("Pull Repo"){
-            ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'pull_repo.yml'
+            ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'pull_repo.yml'
         }
     }
      
     stage("Install Python"){
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'install_python.yml'
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'install_python.yml'
     }  
     stage("Start App"){
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'start_app.yml'
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master-key', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'start_app.yml'
     } 
 }
