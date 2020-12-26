961fcc804166a07b6c01ddfe3c8c1c64258dc65e
azat <azatm@gmail.com>
2020-12-26 19:36:18 +0000

    use func

--- ansible-flaskex-func.groovy
+++ ansible-flaskex-func.groovy
@@ -11,7 +11,19 @@ node{
         git url: 'https://github.com/ikambarov/ansible-Flaskex.git'
     }
     stage("Install Prerequisites"){
-       myplaybook("prerequisites.yml")
+       myplaybook('prerequisites.yml')
     }
+    withEnv(['FLASKEX_REPO=https://github.com/ikambarov/Flaskex.git', 'FLASKEX_BRANCH=master']) {
+        stage("Pull Repo"){
+            myplaybook ('pull_repo.yml')
+        }
+    }
+     
+    stage("Install Python"){
+        myplaybook ('install_python.yml')
+    }  
+    stage("Start App"){
+        myplaybook ('start_app.yml')
+    } 
      
 }
